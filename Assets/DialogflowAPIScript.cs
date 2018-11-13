2103cb1c4a2f8790a9e8f1b30e54fb1fa4c70ff7
Lin Zhang <lin.zhang.ux@gmail.com>
2018-11-13 14:37:59 -0500

    Add unity project with google dialogflow packages

---
+++ Assets/DialogflowAPIScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Networking;
+//using Google.Apis.Dialogflow.v2;
+
+public class DialogflowAPIScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [Serializable]
+    public class TokenClassName
+    {
+        public string access_token;
+    }
+
+    private static IEnumerator GetAccessToken(Action<string> result)
+    {
+        Dictionary<string, string> content = new Dictionary<string, string>();
+        //Fill key and value
+        content.Add("grant_type", "client_credentials");
+        content.Add("client_id", "login-secret");
+        content.Add("client_secret", "secretpassword");
+
+        UnityWebRequest www = UnityWebRequest.Post("https://someurl.com//oauth/token", content);
+        //Send request
+        yield return www.SendWebRequest();
+
+        if (!www.isNetworkError)
+        {
+            string resultContent = www.downloadHandler.text;
+            TokenClassName json = JsonUtility.FromJson<TokenClassName>(resultContent);
+
+            //Return result
+            result(json.access_token);
+        }
+        else
+        {
+            //Return null
+            result("");
+        }
+    }
+}
