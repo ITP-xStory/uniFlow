1f3c53c2fc9bccecf53f5062c75ebf7b51bec4bd
Lin Zhang <lin.zhang.ux@gmail.com>
2018-11-27 12:58:41 -0500

    Add text query dialogflow api call

--- Assets/DialogflowAPIScript.cs
+++ Assets/DialogflowAPIScript.cs
@@ -3,49 +3,84 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.Networking;
+using JsonData;
+
 //using Google.Apis.Dialogflow.v2;
 
 public class DialogflowAPIScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	}
+        // AccessToken is being generated manually in terminal
+        //StartCoroutine(GetAgent(""));
+
+        //https://stackoverflow.com/questions/51272889/unable-to-send-post-request-to-dialogflow-404
+        //first param is the dialogflow API call, second param is Json web token
+        StartCoroutine(PostRequest("https://dialogflow.googleapis.com/v2/projects/",
+                                  ""));
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    [Serializable]
-    public class TokenClassName
-    {
-        public string access_token;
-    }
+    IEnumerator PostRequest(String url, String AccessToken){
+        UnityWebRequest postRequest = new UnityWebRequest(url, "POST");
+        RequestBody requestBody = new RequestBody();
+        requestBody.queryInput = new QueryInput();
+        requestBody.queryInput.text = new TextInput();
+        requestBody.queryInput.text.text = "hello";
+        requestBody.queryInput.text.languageCode = "en";
 
-    private static IEnumerator GetAccessToken(Action<string> result)
-    {
-        Dictionary<string, string> content = new Dictionary<string, string>();
-        //Fill key and value
-        content.Add("grant_type", "client_credentials");
-        content.Add("client_id", "login-secret");
-        content.Add("client_secret", "secretpassword");
+        string jsonRequestBody = JsonUtility.ToJson(requestBody,true);
+        Debug.Log(jsonRequestBody);
 
-        UnityWebRequest www = UnityWebRequest.Post("https://someurl.com//oauth/token", content);
-        //Send request
-        yield return www.SendWebRequest();
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonRequestBody);
+       //Debug.Log(bodyRaw);
+        postRequest.SetRequestHeader("Authorization", "Bearer " + AccessToken);
+        postRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        postRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        //postRequest.SetRequestHeader("Content-Type", "application/json");
 
-        if (!www.isNetworkError)
+        yield return postRequest.SendWebRequest();
+
+        if(postRequest.isNetworkError || postRequest.isHttpError)
         {
-            string resultContent = www.downloadHandler.text;
-            TokenClassName json = JsonUtility.FromJson<TokenClassName>(resultContent);
-
-            //Return result
-            result(json.access_token);
+            Debug.Log(postRequest.responseCode);
+            Debug.Log(postRequest.error);
         }
         else
         {
-            //Return null
-            result("");
+            // Show results as text
+            Debug.Log("Response: " + postRequest.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = postRequest.downloadHandler.data;
+        }
+    }
+    IEnumerator GetAgent(String AccessToken)
+    {
+
+        UnityWebRequest www = UnityWebRequest.Get("https://dialogflow.googleapis.com/v2/projects/test-67717/agent");
+        www.SetRequestHeader("Authorization", "Bearer " + AccessToken);
+
+        yield return www.SendWebRequest();
+        //myHttpWebRequest.PreAuthenticate = true;
+        //myHttpWebRequest.Headers.Add("Authorization", "Bearer " + AccessToken);
+        //myHttpWebRequest.Accept = "application/json";
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
         }
     }
 }
---
+++ Assets/Plugins/JsonData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+namespace JsonData
+{
+     /*
+      "queryParams": {
+        object (QueryParameters)
+    },
+      "queryInput": {
+        object (QueryInput)
+      },
+      "inputAudio": string
+    }*/
+
+    //https://dialogflow.com/docs/reference/api-v2/rest/v2/projects.agent.sessions/detectIntent#QueryParameters
+    [Serializable]
+    public class RequestBody
+    {
+        public QueryInput queryInput;
+        
+    }
+
+    /*
+     * Required request body
+     * queryInput
+     * Represents the query input. It can contain either:
+     * 1.An audio config which instructs the speech recognizer how to process the speech audio.
+     * 2.A conversational query in the form of text,.
+     * 3.An event that specifies which intent to trigger.
+     * JSON representation
+        {
+          // Union field input can be only one of the following:
+          "audioConfig": {
+            object(InputAudioConfig)
+          },
+          "text": {
+            object(TextInput)
+          },
+          "event": {
+            object(EventInput)
+          }
+          // End of list of possible types for union field input.
+        }*/
+    [Serializable]
+    public class QueryInput{
+        public TextInput text;
+    }
+    /*
+     * InputAudioConfig
+     * Instructs the speech recognizer how to process the audio content.
+    {
+      "audioEncoding": enum(AudioEncoding),
+      "sampleRateHertz": number,
+      "languageCode": string,
+      "phraseHints": [
+        string
+      ]
+    }*/
+    [Serializable]
+    public class InputAudioConfig
+    {
+        public String audioEncoding;
+        public int sampleReateHertz;
+        public String languageCode;
+        public String[] phraseHints;
+    }
+    /*AudioEncoding
+     * Audio encoding of the audio content sent in the conversational query request. 
+     * Refer to the Cloud Speech API documentation for more details.
+     */
+
+    public enum AudioEncoding{
+        AUDIO_ENCODING_UNSPECIFIED,
+        AUDIO_ENCODING_LINEAR_16,
+        AUDIO_ENCODING_FLAC,
+        AUDIO_ENCODING_MULAW,
+        AUDIO_ENCODING_AMR,
+        AUDIO_ENCODING_AMR_WB,
+        AUDIO_ENCODING_OGG_OPUS,
+        AUDIO_ENCODING_SPEEX_WITH_HEADER_BYTE
+    }
+
+    /*
+     * TextInput
+     * Represents the natural language text to be processed.
+     * {
+        "text": string,
+        "languageCode": string
+        }*/
+    [Serializable]
+    public class TextInput
+    {
+        public String text;
+        public String languageCode;
+    }
+}
